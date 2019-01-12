bcc80033b7a1e65422dd711e8a0f0add1d340408
John McKay <adenosine3p@gmail.com>
2019-01-12 08:23:50 +0000

    restore terminal on exit
    defer() doesn't get called if readStdin never returns even if main does
    so we have to move the terminal mode setting code outside. Bubbling up
    the exit code is not ideal, but os.Exit() doesn't call the defered
    functions either.

--- gutimer.go
+++ gutimer.go
@@ -32,16 +32,34 @@ func main() {
 		fmt.Printf("Duration: %v\n", duration)
 	}
 	c := make(chan byte)
-	go readStdin(c)
-	runTimer(mode, duration, c)
+	e := make(chan int)
+
+	// put terminal into cbreak mode so we get characters as they are entered
+	t, err := term.Open("/dev/tty")
+	if err != nil {
+		fmt.Printf("Unable to open terminal: %v\n", err)
+		os.Exit(1)
+	}
+	err = t.SetCbreak()
+	if err != nil {
+		fmt.Printf("Unable to set cbreak mode in terminal: %v\n", err)
+		os.Exit(1)
+	}
+	defer t.Restore()
+
+	go readStdin(c, e)
+	ret := runTimer(mode, duration, c, e)
+	t.Restore()
+	os.Exit(ret)
 }
 
 // TODO: add countdown mode
-func runTimer(mode Mode, duration time.Duration, c chan byte) {
+func runTimer(mode Mode, duration time.Duration, c chan byte, e chan int) int {
 	if mode != TIMER && mode != STOPWATCH {
 		fmt.Println("Countdown mode is not supported!")
-		return
+		return 1
 	}
+
 	start := time.Now()
 	if mode == STOPWATCH {
 		duration = 1<<63 - 1 // duration is really an int64
@@ -75,52 +93,32 @@ LOOP:
 					pause = false
 				}
 			}
+		case ret := <-e:
+			return ret
 		}
 	}
 	fmt.Printf("\a\rElapsed time: %v    \n", duration.Round(time.Millisecond))
+	return 0
 }
 
-func readStdin(c chan byte) {
+func readStdin(c chan byte, e chan int) {
 	b := make([]byte, 1)
-	ret := 0
-
-	// put terminal into cbreak mode so we get characters as they are entered
-	t, err := term.Open("/dev/tty")
-	if err != nil {
-		fmt.Printf("Unable to open terminal: %v\n", err)
-		ret = 1
-		goto EXIT
-	}
-	defer t.Restore()
-	err = t.SetCbreak()
-	if err != nil {
-		fmt.Printf("Unable to set cbreak mode in terminal: %v\n", err)
-		ret = 1
-		goto EXIT
-	}
 
 	for {
 		_, err := os.Stdin.Read(b)
 		if err != nil {
 			fmt.Printf("Error reading stdin: %v\n", err)
-			ret = 1
-			goto EXIT
+			e <- 1
 		}
 		if flags.verbose {
 			fmt.Printf("read %q from stdin\n", b[0])
 		}
 		// exit if C-d recieved
 		if b[0] == '\x04' {
-			goto EXIT
+			e <- 0
 		}
 		c <- b[0]
 	}
-
-EXIT:
-	if t != nil {
-		t.Restore()
-	}
-	os.Exit(ret)
 }
 
 func parseFlags() (Mode, time.Duration) {
